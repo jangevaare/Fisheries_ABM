2f7902875cec6e0e3f38b04d6f4b5f1c8bb6d477
jangevaa <jangevaa@uoguelph.ca>
2014-10-02 18:17:21 -0400

    Define initial scope of core files

---
+++ environment.R
@@ -0,0 +1,38 @@
+###########################################################
+# environment.R
+# October 2014
+# Justin Angevaare
+###########################################################
+
+###########################################################
+# SCOPE:
+# This file will define the environment in which the
+# simulated fish move through their life cycle in space and
+# time. These environmental parameters may serve as
+# covariates regarding the mortality or movement of 
+# individuals. The environment will be describe through a
+# series of masks.
+###########################################################
+
+###########################################################
+# HABITAT PREFERABILITY:
+# Habitat preferability will have an effect on the movement 
+# of juveniles as well as the natural mortality of all life
+# stages. This habitat preferability is independent of any
+# anthropogenic effects otherwise included in the model.
+# Habitat preferability is also assumed independent of pre-
+# recruit life stage (i.e. is the same for all individuals)
+###########################################################
+
+###########################################################
+# ANTHROPOGENIC EFFCTS:
+# Anthropogenic effects will add special mortality 
+# covariates to the individuals occupying the areas in
+# which they occur.
+###########################################################
+
+###########################################################
+# CURRENT:
+# Will provide information on the approximate movement of 
+# larvae and juveniles due to water currents. 
+###########################################################
---
+++ individual_events.R
@@ -0,0 +1,30 @@
+###########################################################
+# individual_events.R
+# October 2014
+# Justin Angevaare
+###########################################################
+
+###########################################################
+# SCOPE:
+# The functions defined in this file will be used to apply
+# mortality, movement, and growth at the individual level 
+# to eggs, larvae, and juvenille fish. These events will be
+# tracked using an event-sourcing methodology through
+# discrete time (week level) and space (5' x 5' grid)
+###########################################################
+
+###########################################################
+# NATURAL MORTALITY (LOCATION, STAGE)
+###########################################################
+
+###########################################################
+# OTHER ANTHROPOGENIC MORTALITY (LOCATION, STAGE)
+###########################################################
+
+###########################################################
+# STAGE ADVANCEMENT (STAGE, TIME)
+###########################################################
+
+###########################################################
+# MOVEMENT (LOCATION, STAGE)
+###########################################################
