54329a16899726644fe03d530f8fa489ab24d4a7
jangevaa <jangevaa@uoguelph.ca>
2014-10-02 18:18:57 -0400

    More organization

---
+++ examples.R
@@ -0,0 +1,11 @@
+###########################################################
+# examples.R
+# October 2014
+# Justin Angevaare
+###########################################################
+
+###########################################################
+# SCOPE:
+# This file will be utilized to test core functions as well
+# as to provide a total example of a life cycle simulation
+###########################################################
