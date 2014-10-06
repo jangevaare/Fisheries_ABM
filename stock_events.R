7956ea36574939a4e5f5399cfd496dcccfb0afc1
jangevaa <jangevaa@uoguelph.ca>
2014-10-06 15:28:38 -0400

    Some clean up

--- stock_events.R
+++ stock_events.R
@@ -37,19 +37,20 @@ N[,1]=c(rpois(1, 30000), rpois(1, 20000), rpois(1, 15000), rpois(1, 10000), rpoi
 ###########################################################
 # EGG PRODUCTION
 ###########################################################
-Egg_Production=function(Age_Matrix, prop_mature, egg_num, t){
-	 c(rpois(rbinom(1, Age_Matrix[1, t], 0.5*prop_mature[1]), egg_num[1]),
-	   rpois(rbinom(1, Age_Matrix[2, t], 0.5*prop_mature[2]), egg_num[2]), 
-	   rpois(rbinom(1, Age_Matrix[3, t], 0.5*prop_mature[3]), egg_num[3]),
-	   rpois(rbinom(1, Age_Matrix[4, t], 0.5*prop_mature[4]), egg_num[4]),
-	   rpois(rbinom(1, Age_Matrix[5, t], 0.5*prop_mature[5]), egg_num[5]))}
+Egg_Production=function(Age_Matrix, prop_mature, egg_num, time){
+	 c(rpois(rbinom(1, Age_Matrix[1, time], 0.5*prop_mature[1]), egg_num[1]),
+	   rpois(rbinom(1, Age_Matrix[2, time], 0.5*prop_mature[2]), egg_num[2]), 
+	   rpois(rbinom(1, Age_Matrix[3, time], 0.5*prop_mature[3]), egg_num[3]),
+	   rpois(rbinom(1, Age_Matrix[4, time], 0.5*prop_mature[4]), egg_num[4]),
+	   rpois(rbinom(1, Age_Matrix[5, time], 0.5*prop_mature[5]), egg_num[5]))}
 
 ###########################################################
 # AGE
 ###########################################################
-Adult_Update_pt1=function(Age_Matrix, survivorship, t){
-	Age_Matrix[, t+1] = c(0, # Set as zero until entered from ABM	
-						rbinom(1, Age_Matrix[1,t], survivorship[1]),
+Adult_Update_pt1=function(Age_Matrix, survivorship, event_db, time){
+	Age_Matrix[, t+1] = c(event_db$num_alive[(event_db$time==time) & 
+	                     (event_db$stage=='adult'),],
+	                    rbinom(1, Age_Matrix[1,t], survivorship[1]),
 						rbinom(1, Age_Matrix[2,t], survivorship[2]),
 						rbinom(1, Age_Matrix[3,t], survivorship[3]),
 						rbinom(1, Age_Matrix[4,t], survivorship[4]) +
